de6ccfcf7d5506845cbd1a6912731d96fefb5309
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-02 07:56:42 -0700

    Added books#new, create, edit and update actions and templates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,18 +30,60 @@ class BooksController < ApplicationController
     end
   end
 
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(
+      author: params[:book][:author],
+      title: params[:book][:title],
+      description: params[:book][:description]
+    )
+
+    if @book.save
+      redirect_to books_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  def edit
+    book_id = params[:id].to_i
+    @book = Book.find_by(id: book_id)
+    if @book.nil?
+      head :not_found # only HTTP headers should be sent back (no <body, no payload)
+      return
+    end
+  end
+
+  def update
+    book_id = params[:id].to_i
+    @book = Book.find_by(id: book_id)
+    if @book.nil?
+      head :not_found
+      return
+    elsif @book.update(
+      author: params[:book][:author], 
+      title: params[:book][:title], 
+      description: params[:book][:description]
+    )
+      redirect_to books_path # go to the index so we can see the book in the list
+      return
+    else # save failed :(
+      render :edit # show the new book form view again
+      return
+    end
+
+
+    # @book.author = params[:book][:author]
+    # @book.title = params[:book][:title]
+    # @book.description = params[:book][:description]
+    # @book.save
+  end
+
+  def destroy
   end
 end
